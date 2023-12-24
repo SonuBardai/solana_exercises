3b238cb4ee3be3e4dafef56c087a9aed7fc783fa
SonuBardai <sonubardai9@gmail.com>
2023-12-24 14:24:43 +0530

    config functions to read keys from env

--- 3transferSol.ts
+++ 3transferSol.ts
@@ -1,13 +1,5 @@
-import {
-  Connection,
-  Keypair,
-  LAMPORTS_PER_SOL,
-  PublicKey,
-  SystemProgram,
-  Transaction,
-  sendAndConfirmTransaction,
-} from "@solana/web3.js";
-import { ADDRESS } from "./config";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
+import { ADDRESS, getPublicKeyFromEnv, getYourKeypairFromEnv } from "./config";
 import { config } from "dotenv";
 
 const transferSol = async (from: Keypair, to: PublicKey, lamports: number) => {
@@ -20,25 +12,15 @@ const transferSol = async (from: Keypair, to: PublicKey, lamports: number) => {
   });
   transaction.add(instruction);
   await sendAndConfirmTransaction(connection, transaction, [from]);
-  console.log(`Transferred ${lamports/LAMPORTS_PER_SOL}SOL from ${from.publicKey} to ${to}`);
+  console.log(`Transferred ${lamports / LAMPORTS_PER_SOL}SOL from ${from.publicKey} to ${to}`);
 };
 
 const main = async () => {
   config();
 
-  const fromSecretKeyString = process.env.YOUR_KEY || "";
-  const toSecretKeyString = process.env.OTHER_KEY || "";
-  if (!fromSecretKeyString || !toSecretKeyString) {
-    throw new Error(
-      "Environment variables YOUR_KEY and OTHER_KEY must be set."
-    );
-  }
-  const fromSecretKey = Uint8Array.from(JSON.parse(fromSecretKeyString));
-  const toSecretKey = Uint8Array.from(JSON.parse(toSecretKeyString));
-  const fromKeyPair = Keypair.fromSecretKey(fromSecretKey);
-  const toKeyPair = Keypair.fromSecretKey(toSecretKey);
-
-  await transferSol(fromKeyPair, toKeyPair.publicKey, 2 * LAMPORTS_PER_SOL);
+  const fromKeyPair = getYourKeypairFromEnv();
+  const toPublicKey = getPublicKeyFromEnv("OTHER_KEY");
+  await transferSol(fromKeyPair, toPublicKey, 2 * LAMPORTS_PER_SOL);
 
   console.log("Done");
 };
--- config.ts
+++ config.ts
@@ -1,4 +1,31 @@
+import { Keypair, PublicKey } from "@solana/web3.js";
+
 export const ADDRESS = "http://localhost:8899";
 export const PUBLIC_KEY = "5byepJy3Yc3hqgrYJpaybCzcfMDZ2uympY4Rf84ATbWv";
 
 export const PUBLIC_KEY_2 = "EDJNYgaQhkVfohykLTh2yo4LxStqX233cNPGku8FFNmH";
+
+export const getKeypairFromEnv = (keyName: string) => {
+  const rawKey = process.env[keyName] || "";
+  if (!rawKey) {
+    throw new Error(`Environment variable for ${keyName} must be set.`);
+  }
+  const parsedKey = Uint8Array.from(JSON.parse(rawKey));
+  const keyPair = Keypair.fromSecretKey(parsedKey);
+  return keyPair;
+};
+
+export const getYourKeypairFromEnv = (): Keypair => {
+  return getKeypairFromEnv("YOUR_KEY");
+};
+
+export const getPublicKeyFromEnv = (keyName: string): PublicKey => {
+  try {
+    const keyPair = getKeypairFromEnv("OTHER_KEY");
+    return keyPair.publicKey;
+  } catch (error) {
+    const rawKey = process.env[keyName] || "";
+    const publicKey = new PublicKey(rawKey);
+    return publicKey;
+  }
+};
